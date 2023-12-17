66c30f411af56dabef6686ccb9b5741e3903b0a1
Abdullah Ali <23sw082@students.muet.edu.pk>
2023-12-17 21:50:17 +0500

    calculator

---
+++ calculator.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class Calculator
+{
+private:
+float number_1;
+float number_2;
+float result;
+public:
+void set_number1(float a);
+void set_number2(float a);
+void set_result(float a);
+float get_result();
+void opt(char opt);
+};
+void Calculator:: opt(char opt)
+{
+    switch(opt)
+    {
+        case '*':{result=number_1*number_2;
+        cout<<number_1<<" * "<<number_2<<" = "<<result<<endl;
+        }break;
+        case '/':{result=number_1/number_2;
+        cout<<number_1<<" / "<<number_2<<" = "<<result<<endl;
+        }break;
+        case '+':{result=number_1+number_2;
+        cout<<number_1<<" + "<<number_2<<" = "<<result<<endl;
+        }break;
+        case '-':{result=number_1-number_2;
+        cout<<number_1<<" - "<<number_2<<" = "<<result<<endl;
+        }break;
+        
+    }
+}
+void Calculator:: set_number1(float a){number_1=a;}
+void Calculator:: set_number2(float a){number_2=a;}
+void Calculator:: set_result(float a){result=a;}
+float Calculator:: get_result(){return result;}
+int main()
+{
+    float no_1,no_2;
+    char opt,option;
+    do{
+    Calculator a;
+    cout<<"Enter first number: ";
+    cin>>no_1;
+    a.set_number1(no_1);
+    cout<<"Enter secound number: ";
+    cin>>no_2;
+    a.set_number2(no_2);
+    cout<<"Enter operator: ";
+    cin>>opt;
+    a.opt(opt);
+    cout<<"Do you have more calculations to do..";
+    cout<<"Enter \'y\' to continoue";
+    cin>>option;
+    }while(option=='y');
+
+ return 0;
+}
