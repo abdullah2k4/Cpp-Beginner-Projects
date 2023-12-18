b1514e6bcea440543c9765c7ecd3d49fb032c7ef
Abdullah Ali <23sw082@students.muet.edu.pk>
2023-12-18 13:04:20 +0500

    update to divide funtion

--- calculator.cpp
+++ calculator.cpp
@@ -3,62 +3,9 @@ using namespace std;
 class Calculator
 {
 private:
-<<<<<<< HEAD
     float number_1;
     float number_2;
     float result;
-=======
-float number_1;
-float number_2;
-float result;
-public:
-void set_number1(float a);
-void set_number2(float a);
-void set_result(float a);
-void opt(char opt);
-};
-void Calculator:: opt(char opt)
-{
-    switch(opt)
-    {
-        case '*':{result=number_1*number_2;
-        cout<<number_1<<" * "<<number_2<<" = "<<result<<endl;
-        }break;
-        case '/':{result=number_1/number_2;
-        cout<<number_1<<" / "<<number_2<<" = "<<result<<endl;
-        }break;
-        case '+':{result=number_1+number_2;
-        cout<<number_1<<" + "<<number_2<<" = "<<result<<endl;
-        }break;
-        case '-':{result=number_1-number_2;
-        cout<<number_1<<" - "<<number_2<<" = "<<result<<endl;
-        }break;
-        
-    }
-}
-void Calculator:: set_number1(float a){number_1=a;}
-void Calculator:: set_number2(float a){number_2=a;}
-void Calculator:: set_result(float a){result=a;}
-int main()
-{
-    float no_1,no_2;
-    char opt,option;
-    do{
-    Calculator a;
-    cout<<"Enter first number: ";
-    cin>>no_1;
-    a.set_number1(no_1);
-    cout<<"Enter secound number: ";
-    cin>>no_2;
-    a.set_number2(no_2);
-    cout<<"Enter operator: ";
-    cin>>opt;
-    a.opt(opt);
-    cout<<"Do you have more calculations to do..";
-    cout<<"Enter \'y\' to continoue";
-    cin>>option;
-    }while(option=='y');
->>>>>>> 2b693370a74c955ac4e955ac4ef32c4b500e7978
 
 public:
     void set_number1(float a);
@@ -79,7 +26,7 @@ void Calculator::opt(char opt)
     {
         if (number_2 == 0 && number_1 != 0)
         {
-            cout << "Infitity\n";break;
+            cout << "Infinity\n";break;
         }
     
         if (number_2 == 0 && number_1 == 0)
