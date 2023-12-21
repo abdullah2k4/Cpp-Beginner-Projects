3c9cb176092f0558d1b59c9c45bbd89d894693eb
Abdullah Ali <23sw082@students.muet.edu.pk>
2023-12-21 19:12:41 +0500

    Number-guessing

--- Number-guessing.cpp
+++ Number-guessing.cpp
@@ -1,33 +1,35 @@
-#include<iostream>
-#include<conio.h>
-// #include<cstdlib>
+#include <iostream>
+#include <conio.h>
 using namespace std;
 int main()
 {
-char option;
- do{
-int guess,attempt=1;
- int number;
- number=rand()%100;
-    system("clear");
- do{
- cout<<"Attempt number "<<attempt<<"\nGuess the number between 1 to 100 : ";
- cin>>guess;
- if(number<guess)
- {
-    cout<<"The number is less than your gussed number\n";
-    ++attempt;
- }
- else if(number>guess)
- {
-    cout<<"The number is greater than your gussed number\n";
-    ++attempt;
- }
- }while(number!=guess);
- cout<<"Congratulation guessed the number in "<<attempt<<" attempt!!\n";
- cout<<"Press y to play again : ";
- cin>>option;
- }
- while(option=='y');
- return 0;
+   char option;
+   do
+   {
+      int guess, attempt = 1;
+      int number;
+      number = (rand() % 100) + 1;
+      system("clear");
+      do
+      {
+         cout << "Attempt number " << attempt << "\nGuess the number between 1 to 100 : ";
+         cin >> guess;
+         if (number < guess)
+         {
+            system("cls");
+            cout << "The number is less than your gussed number\n";
+            ++attempt;
+         }
+         else if (number > guess)
+         {
+            system("cls");
+            cout << "The number is greater than your gussed number\n";
+            ++attempt;
+         }
+      } while (number != guess);
+      cout << "Congratulation guessed the number in " << attempt << " attempt!!\n";
+      cout << "Press y to play again : ";
+      cin >> option;
+   } while (option == 'y');
+   return 0;
 }
