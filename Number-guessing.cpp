e80d304fefabc0fbb654f007da187b28256b9673
Abdullah Ali <23sw082@students.muet.edu.pk>
2024-01-02 22:22:18 +0500

    modification

--- Number-guessing.cpp
+++ Number-guessing.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
-#include <conio.h>
-using namespace std;
+#include <cstdlib>
+#include <ctime>
+
 int main()
 {
-   char option;
-   do
-   {
-      int guess, attempt = 1;
-      int number;
-      number = (rand() % 100) + 1;
-      system("clear");
-      do
-      {
-         cout << "Attempt number " << attempt << "\nGuess the number between 1 to 100 : ";
-         cin >> guess;
-         if (number < guess)
-         {
-            system("cls");
-            cout << "The number is less than your gussed number\n";
-            ++attempt;
-         }
-         else if (number > guess)
-         {
-            system("cls");
-            cout << "The number is greater than your gussed number\n";
-            ++attempt;
-         }
-      } while (number != guess);
-      cout << "Congratulation guessed the number in " << attempt << " attempt!!\n";
-      cout << "Press y to play again : ";
-      cin >> option;
-   } while (option == 'y');
-   return 0;
+    char option;
+    srand(time(0));
+
+    do
+    {
+        int guess, attempt = 1;
+        int number = rand() % 100 + 1;
+
+
+        do
+        {
+            std::cout << "Attempt number " << attempt << "\nGuess the number between 1 to 100: ";
+            std::cin >> guess;
+
+            if (number < guess)
+            {
+                std::cout << "The number is less than your guessed number\n";
+                ++attempt;
+            }
+            else if (number > guess)
+            {
+                std::cout << "The number is greater than your guessed number\n";
+                ++attempt;
+            }
+
+        } while (number != guess);
+
+        std::cout << "Congratulations! Guessed the number in " << attempt << " attempt!!\n";
+        std::cout << "Press 'y' to play again: ";
+        std::cin >> option;
+        system("cls");
+    } while (option == 'y');
+
+    return 0;
 }
